1d33b00d45119904ccff4157fa7d0def386d95bc
Kadoia614 <miguel_moraes2001@hotmail.com>
2025-04-10 15:46:29 -0300

    Refactor createRole function to destructure name from request body and include it in the response

--- controller/Permissions/rolesController.js
+++ controller/Permissions/rolesController.js
@@ -1,10 +1,10 @@
 const service_api = require("../../service/service_api"); 
 
 const createRole = async (request, reply) =>{
-    let name = request.body.name.role;
+    let {name} = request.body.name.role;
     const services = await service_api.get('/service')
     console.log(services)
-    reply.status(200).send({teste: services})
+    reply.status(200).send({teste: services, name})
 }
 
 module.exports = {
