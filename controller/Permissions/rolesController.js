da296bdddd8ff582196d3ff01b459f2f81ad7e46
Kadoia614 <miguel_moraes2001@hotmail.com>
2025-04-10 15:50:43 -0300

    Fix service response handling in createRole function

--- controller/Permissions/rolesController.js
+++ controller/Permissions/rolesController.js
@@ -2,7 +2,8 @@ const service_api = require("../../service/service_api");
 
 const createRole = async (request, reply) =>{
     let {name} = request.body.role;
-    const services = await service_api.get('/service')
+    const servicesResponse = await service_api.get('/service');
+    const services = servicesResponse.data;
     console.log(services)
     reply.status(200).send({teste: services, name})
 }
