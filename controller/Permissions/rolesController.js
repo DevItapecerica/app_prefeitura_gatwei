12ff56f157042542bfa39a07c85ed1f10579d794
Kadoia614 <miguel_moraes2001@hotmail.com>
2025-04-10 15:47:33 -0300

    Fix destructuring of role name in createRole function

--- controller/Permissions/rolesController.js
+++ controller/Permissions/rolesController.js
@@ -1,7 +1,7 @@
 const service_api = require("../../service/service_api"); 
 
 const createRole = async (request, reply) =>{
-    let {name} = request.body.name.role;
+    let {name} = request.body.role;
     const services = await service_api.get('/service')
     console.log(services)
     reply.status(200).send({teste: services, name})
