a942270e2ad04d5bce9d53109ec80088713222b7
Kadoia614 <miguel_moraes2001@hotmail.com>
2025-04-10 16:00:02 -0300

    Add role creation functionality to rolesController

--- controller/Permissions/rolesController.js
+++ controller/Permissions/rolesController.js
@@ -1,11 +1,19 @@
 const service_api = require("../../service/service_api"); 
+const roles_api = require("../../service/permissions_api");
 
 const createRole = async (request, reply) =>{
     let {name} = request.body.role;
     const servicesResponse = await service_api.get('/service');
     const services = servicesResponse.data;
+
+    await roles_api.post({
+        role: {
+            name: name,
+        },
+        services: services
+    })
     console.log(services)
-    reply.status(200).send({teste: services, name})
+    reply.status(201).send('Created role');
 }
 
 module.exports = {
