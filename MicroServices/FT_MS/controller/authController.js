f31f4394517a61c10a14aab96ebc144d799211d2
Kadoia614 <miguel_moraes2001@hotmail.com>
2025-05-14 09:37:55 -0300

    Fix token generation error handling in getRandomToken function

--- MicroServices/FT_MS/controller/authController.js
+++ MicroServices/FT_MS/controller/authController.js
@@ -5,14 +5,19 @@ const TOKEN_EXPIRATION = "1h"; // Token expiration time
 
 const getRandomToken = async (request, reply) => {
   try {
-  } catch (error) {}
-  const { id } = request.params;
-  const payload = {
-    user: id,
-    timestamp: Date.now(),
-  };
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: TOKEN_EXPIRATION });
-  return reply.send({ token });
+    const { id } = request.params;
+    const payload = {
+      user: id,
+      timestamp: Date.now(),
+    };
+    const token = jwt.sign(payload, SECRET_KEY, {
+      expiresIn: TOKEN_EXPIRATION,
+    });
+    return reply.send({ token });
+  } catch (error) {
+    error.message += " Error generating token";
+    throw error;
+  }
 };
 
 module.exports = {
