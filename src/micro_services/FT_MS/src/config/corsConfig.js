8e5cab7b187716f03136621671f60e94a37a07d8
Kadoia614 <miguel_moraes2001@hotmail.com>
2025-09-15 15:43:05 -0300

    removido Frente de trabalho para que aja como sistema separado

--- src/micro_services/FT_MS/src/config/corsConfig.js
+++
@@ -1,8 +0,0 @@
-const corsConfig = {
-    origin: "*",
-    allowedHeaders: ["Content-Type", "x-api-key", "x-access-token"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-}
-
-export default corsConfig;
--- src/micro_services/FT_MS/src/utils/calcularVencimentoBolsista.js
+++
@@ -1,5 +0,0 @@
-export function calcularDataVencimento(dataVinculo, prorrogado) {
-  const venc = new Date(dataVinculo);
-  venc.setFullYear(venc.getFullYear() + (prorrogado ? 2 : 1)); // Ex: prorrogação dá +1 ano
-  return venc;
-}
