20347cb3cdbe6bfe3b94dcd3b0ca07508be9feb5
Kadoia614 <miguel_moraes2001@hotmail.com>
2026-02-23 08:48:52 -0300

    Fix: Fix import into router

--- src/router/router.js
+++ src/router/router.js
@@ -3,8 +3,8 @@ const Router = (fastify) => {
   fastify.register(import("./setorRouter.js"), { prefix: "/setor" });
   fastify.register(import("./serviceRouter.js"), { prefix: "/service" });
   fastify.register(import("./roleRouter.js"), { prefix: "/roles" });
-  fastify.register(import("./ft/bolsistaRouter.js"), { prefix: "/ft/bolsista" });
-  fastify.register(import("./ft/editalRouter.js"), { prefix: "/ft/edital" });
+  fastify.register(import("./FT/bolsistaRouter.js"), { prefix: "/ft/bolsista" });
+  fastify.register(import("./FT/editalRouter.js"), { prefix: "/ft/edital" });
   fastify.register(import("./authenticateRouter.js"), { prefix: "/auth" });
 
   fastify.register(import("./reservas/reservasRouter.js"), { prefix: "/reservas" });
